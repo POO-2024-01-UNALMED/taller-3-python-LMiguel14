2688b15b722b0e68549c6b6f991764cc81af1143
LMiguel14 <159493106+LMiguel14@users.noreply.github.com>
2024-02-26 00:37:21 -0500

    Update control.py

---
+++ televisores/control.py
@@ -0,0 +1,11 @@
+class Control():
+    tv = None
+    
+    def enlazar(self, televisor):
+        self.tv = televisor
+    def getTv(self):
+        return self.tv
+    def setTv(self,televisor):
+        self.tv = televisor
+
+
