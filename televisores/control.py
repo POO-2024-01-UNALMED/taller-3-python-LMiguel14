ebd13ac91532e767433a732cb1b88adfc14da264
LMiguel14 <159493106+LMiguel14@users.noreply.github.com>
2024-02-26 19:44:43 -0500

    Update control.py

--- televisores/control.py
+++ televisores/control.py
@@ -1,9 +1,9 @@
-from televisores.tv import TV
+from tv import TV
 class Control():
     tv = None
     
     def enlazar(self, televisor):
-        self.tv = TV(televisor)
+        self.tv = televisor
     def getTv(self):
         return self.tv
     def setTv(self,televisor):
@@ -11,26 +11,32 @@ class Control():
 
     def setVolumen(self,volumen):
         if  0 < volumen <=7 and TV.getEstado(self) == True:
-            TV.setVolumen(volumen) 
-
+            TV.setVolumen(volumen)  
     def setCanal(self,canal):
         if  1 < canal <=120  and TV.getEstado(self) == True:
-            TV.setCanal(canal)        
-
+            TV.setCanal(canal)          
     def turnOff(self):
-        TV.turnOff()
+        TV.turnOff(self.tv)
     def turnOn(self):
-        TV.turnOn()
-
+        TV.turnOn(self.tv) 
     def canalDown(self):
-        if  (1 < TV.getCanal() < 120) and  TV.getEstado() == True:
+        if  (1 < TV.getCanal(self.tv) < 120) and  TV.getEstado(self.tv) == True:
             TV.canalDown()        
     def canalUp(self):
-        if (TV.getCanal() < 120) and  TV.getEstado() == True:
+        if (TV.getCanal(self.tv) < 120) and  TV.getEstado(self.tv) == True:
             TV.canalUp()
     def volumenUp(self):
-        if (TV.getVolumen() < 7) and  TV.getEstado() == True:
-            TV.volumenUp()
+        if (TV.getVolumen(self.tv) < 7) and  TV.getEstado(self.tv) == True:
+            TV.volumenUp(self.tv)
     def volumenDown(self):
-        if  (0 < TV.getVolumen() <=7) and  TV.getEstado() == True:
-            TV.volumenDown()
+        if  (0 < TV.getVolumen(self.tv) <=7) and  TV.getEstado(self.tv) == True:
+            TV.volumenDown(self.tv)    
+televisor= TV("lg", True)
+control= Control()
+
+control.enlazar(televisor)
+
+print(televisor.volumen)
+control.volumenDown()
+
+print(televisor.volumen)
